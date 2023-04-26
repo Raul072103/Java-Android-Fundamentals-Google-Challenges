cc113bc412fe4865a6015ac83a33de36f9938de4
raul <raulandrei2017@yahoo.com>
2023-04-26 18:03:36 +0300

    check if email and phone number are ok  added finished

--- Challenge2/app/src/main/java/com/example/challenge2/MainActivity.java
+++ Challenge2/app/src/main/java/com/example/challenge2/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,19 +25,48 @@ public class MainActivity extends AppCompatActivity {
         phoneEditText = findViewById(R.id.phoneEditText);
         submitButton = findViewById(R.id.button);
 
-
+        checkIfEmailAndPhoneAreOK();
     }
 
     private void checkIfEmailAndPhoneAreOK(){
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String phone = phoneEditText.getText().toString();
 
+                boolean check = validateInfo(email, phone);
+
+                    if(check == true){
+                        Toast.makeText(getApplicationContext(), "Data is valid", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Sorry check information again", Toast.LENGTH_LONG).show();
+                    }
             }
+
         });
     }
 
-    private boolean checkIfEmailIsOK(){
+    private boolean validateInfo(String email, String phone) {
+       // email.trim();
+        if(email.length() == 0){
+            emailEditText.requestFocus();
+            emailEditText.setError("Field cannot be empty");
+            return false;
+        }else if(! email.matches("^[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")){
+            emailEditText.requestFocus();
+            emailEditText.setError("Enter a valid email");
+            return false;
+        }else if(phone.length() == 0){
+            emailEditText.requestFocus();
+            emailEditText.setError("Field cannot be empty");
+            return false;
+        }else if(! phone.matches("[0-9]{10,13}$")){
+            phoneEditText.requestFocus();
+            phoneEditText.setError("Enter a valid phone number");
+            return false;
+        }else return true;
 
     }
+
 }
